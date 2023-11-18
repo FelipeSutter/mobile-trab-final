0f70b907a4e4345c8c2eefd06858c418fdd12e54
Felipe Sutter <lipeps61@gmail.com>
2023-11-18 19:35:12 -0300

    Estilização da página detalhes

--- src/pages/Detalhes/index.js
+++ src/pages/Detalhes/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ScrollView } from "react-native";
+import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
 import api from "../../../service/api";
 
@@ -20,16 +20,58 @@ const Detalhes = ({ route }) => {
   }, [id]);
 
   return (
-    <ScrollView>
-      <Image
-        source={{ uri: musica.url }}
-        style={{ width: 100, height: 100, marginTop: 50 }}
-      />
-      <Text style={{ marginTop: 50 }}>{musica.artist}</Text>
-      <Text style={{ marginTop: 50 }}>{musica.title}</Text>
-      <Text style={{ marginTop: 50 }}>{musica.description}</Text>
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.containerInfo}>
+          <Image source={{ uri: musica.url }} style={styles.img} />
+
+          <Text style={[styles.textArtist, styles.text]}>{musica.artist}</Text>
+          <Text style={[styles.textMusic, styles.text]}>{musica.title}</Text>
+          <Text style={[styles.textDescription, styles.text]}>
+            {musica.description}
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#141a35",
+  },
+  containerInfo: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
+    marginTop: 20,
+  },
+  img: {
+    width: 200,
+    height: 200,
+    borderWidth: 2,
+    borderColor: "#b0d6ff",
+    borderRadius: 100,
+  },
+  text: {
+    color: "#e2e2e2",
+    marginTop: 10,
+  },
+  textArtist: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  textMusic: {
+    fontSize: 30,
+    fontWeight: "500",
+  },
+  textDescription: {
+    fontSize: 20,
+  },
+});
 
 export default Detalhes;
